1eb840b09e66f7ba02e627c7c54d814c81fab28f
natalyq1 <onatylili@gmail.com>
2023-04-20 14:16:50 -0500

    component Boton, event prevent default so we can control the action

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,18 +1,26 @@
-import CampoTexto from '../CampoTexto'
-import ListaOpciones from '../ListaOpciones/ListaOpciones'
-import './Formulario.css'
-
+import Boton from "../Boton/Index";
+import CampoTexto from "../CampoTexto";
+import ListaOpciones from "../ListaOpciones/ListaOpciones";
+import "./Formulario.css";
 
 const Formulario = () => {
-    return <section className='formulario'>
-        <form action="">
-            <h2>Rellena el formulario para crear al colaborador</h2>
-            <CampoTexto titulo='Nombre' placeholder='Ingrese su nombre'/>
-            <CampoTexto titulo='Puesto' placeholder='Ingrese su puesto'/>
-            <CampoTexto titulo='Foto' placeholder='Ingresar enlace de foto'/>
-            < ListaOpciones />
-        </form>
-    </section>
-}
 
-export default Formulario
+    const manejarEnvio = (evento) => {
+        evento.preventDefault()
+        console.log('manejar el envio', evento);
+    }
+  return (
+    <section className="formulario">
+      <form action="" onSubmit={manejarEnvio}>
+        <h2>Rellena el formulario para crear al colaborador</h2>
+        <CampoTexto titulo="Nombre" placeholder="Ingrese su nombre" />
+        <CampoTexto titulo="Puesto" placeholder="Ingrese su puesto" />
+        <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" />
+        <ListaOpciones />
+        <Boton>Crear</Boton>
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
